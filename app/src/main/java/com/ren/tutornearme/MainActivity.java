d0d27561e398ab27eb265390ae2e346f61ac2730
M4rm3ladov <renier.antonio20@gmail.com>
2023-10-03 01:38:28 +0800

    user navigate from sign in to register

--- app/src/main/java/com/ren/tutornearme/MainActivity.java
+++ app/src/main/java/com/ren/tutornearme/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthMethodPickerLayout;
@@ -14,18 +15,37 @@ import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.ren.tutornearme.util.Common;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private List<AuthUI.IdpConfig> providers = new ArrayList<>();
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
     private FirebaseUser currentUser;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection(Common.TUTOR_INFO_REFERENCE);
     private ActivityResultLauncher<Intent> signInLauncher;
+    private boolean userExists = false;
 
     @Override
     protected void onStart() {
@@ -54,7 +74,9 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
 
-        providers.add(new AuthUI.IdpConfig.PhoneBuilder().build());
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.PhoneBuilder().setAllowedCountries(Collections.singletonList("ph")).build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
         firebaseAuth = FirebaseAuth.getInstance();
 
         authStateListener = new FirebaseAuth.AuthStateListener() {
@@ -62,17 +84,55 @@ public class MainActivity extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 currentUser = firebaseAuth.getCurrentUser();
                 if (currentUser != null) {
-                    //user logged in
+                    Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                    startActivity(intent);
+                    finish();
                 } else {
                     showLoginLayout();
                 }
             }
         };
     }
+
+    private void saveUserToDatabase(String currentUserId) {
+        Map<String, String> userObj = new HashMap<>();
+        userObj.put("userId", currentUserId);
+
+        collectionReference.add(userObj)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        documentReference.get()
+                            .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                @Override
+                                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                    if (documentSnapshot.exists()) {
+                                        Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                                        intent.putExtra("uid", currentUserId);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+
+                                    }
+                                });
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+    }
+
     private void showLoginLayout() {
         AuthMethodPickerLayout authMethodPickerLayout = new AuthMethodPickerLayout
                 .Builder(R.layout.activity_main)
-                .setPhoneButtonId(R.id.phone_sign_in_button)
+                .setPhoneButtonId(R.id.get_started_button)
+                .setGoogleButtonId(R.id.google_sign_in_button)
                 .build();
 
         signInLauncher.launch(AuthUI.getInstance()
@@ -88,9 +148,7 @@ public class MainActivity extends AppCompatActivity {
         IdpResponse response = result.getIdpResponse();
         if (result.getResultCode() == RESULT_OK) {
             // Successfully signed in
-            currentUser = FirebaseAuth.getInstance().getCurrentUser();
-
-
+            currentUser = firebaseAuth.getCurrentUser();
         } else {
             // Sign in failed. If response is null the user canceled the
             // sign-in flow using the back button. Otherwise check
