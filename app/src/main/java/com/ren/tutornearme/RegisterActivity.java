b0699e9e7c838ffaa5b69b597c98494bdc65e3aa
M4rm3ladov <renier.antonio20@gmail.com>
2023-10-03 18:32:10 +0800

    fixed gender radio group error

--- app/src/main/java/com/ren/tutornearme/RegisterActivity.java
+++ app/src/main/java/com/ren/tutornearme/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.ren.tutornearme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -14,10 +15,18 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.ren.tutornearme.data.AddressBank;
 import com.ren.tutornearme.data.AddressListAsyncResponse;
-import com.ren.tutornearme.model.Address;
+import com.ren.tutornearme.model.TutorInfo;
+import com.ren.tutornearme.util.Common;
 import com.ren.tutornearme.util.InputValidatorHelper;
 
 import java.util.ArrayList;
@@ -27,16 +36,24 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     private EditText firstNameEditText, lastNameEditText;
     private AutoCompleteTextView barangayEditText;
     private RadioGroup genderRadioGroup;
-    private RadioButton maleRadioButton, femaleRadioButton;
+    private RadioButton maleRadioButton, femaleRadioButton, checkedGenderRadioButton;
     private TextInputLayout firstNameInputLayout, lastNameInputLayout, genderInputLayout, barangayInputLayout;
     private Button registerButton;
     private boolean isValid = true;
     private List<String> barangayList;
     private InputValidatorHelper inputValidatorHelper = new InputValidatorHelper();
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection(Common.TUTOR_INFO_REFERENCE);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         firstNameEditText = findViewById(R.id.first_name_edit_text);
         lastNameEditText = findViewById(R.id.last_name_edit_text);
@@ -152,11 +169,14 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
             @Override
             public void afterTextChanged(Editable editable) {
-                String barangayName = barangayEditText.getText().toString().trim();
-                if (!barangayList.contains(barangayName)) {
-                    barangayInputLayout.setHelperText("Please fill in valid Barangay.");
-                    isValid = false;
-                }
+
+            }
+        });
+
+        genderRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int id) {
+                checkedGenderRadioButton = findViewById(id);
             }
         });
     }
@@ -173,6 +193,41 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
     private void saveTutorInfo() {
         validateBeforeSave();
+
+        currentUser = firebaseAuth.getCurrentUser();
+        // check if passed validation and has user
+
+        if (!isValid) return;
+
+        String currentUserUid = currentUser.getUid();
+        String firstName = firstNameEditText.getText().toString().trim();
+        String lastName = lastNameEditText.getText().toString().trim();
+        String gender = checkedGenderRadioButton.getText().toString();
+        String phoneNumber = currentUser.getPhoneNumber();
+        String barangay = barangayEditText.getText().toString().trim();
+
+        TutorInfo tutorInfo = new TutorInfo();
+        tutorInfo.setUid(currentUserUid);
+        tutorInfo.setFirstName(firstName);
+        tutorInfo.setLastName(lastName);
+        tutorInfo.setGender(gender);
+        tutorInfo.setPhoneNumber(phoneNumber);
+        tutorInfo.setAddress(barangay);
+
+        if (currentUser != null) {
+            collectionReference.add(tutorInfo)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(RegisterActivity.this, "Successfully saved information", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(RegisterActivity.this, "[ERROR]: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+        }
     }
 
     private void validateBeforeSave() {
@@ -180,8 +235,13 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         String lastName = lastNameEditText.getText().toString().trim();
         String barangay = barangayEditText.getText().toString().trim();
 
+        if (!barangayList.contains(barangay)) {
+            barangayInputLayout.setHelperText("Please fill in valid Barangay.");
+            isValid = false;
+        }
         if (!maleRadioButton.isChecked() && !femaleRadioButton.isChecked()) {
             genderInputLayout.setHelperText("Please select a gender");
+            isValid = false;
         }
         if (inputValidatorHelper.isNullOrEmpty(firstName)) {
             firstNameInputLayout.setHelperText("Please fill in first name.");
