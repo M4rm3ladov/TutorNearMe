4f6e407f2453dea76a690309fe10e5c27462e70a
M4rm3ladov <renier.antonio20@gmail.com>
2023-10-04 19:18:40 +0800

    added static tutor info static field

--- app/src/main/java/com/ren/tutornearme/util/Common.java
+++ app/src/main/java/com/ren/tutornearme/util/Common.java
@@ -1,5 +1,8 @@
 package com.ren.tutornearme.util;
 
+import com.ren.tutornearme.model.TutorInfo;
+
 public class Common {
     public static final String TUTOR_INFO_REFERENCE = "TutorInfo";
+    public static TutorInfo currentTutor;
 }
