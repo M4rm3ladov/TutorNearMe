bf4b25015cd61460bebdb8b44d4b1dd5a377394d
M4rm3ladov <renier.antonio20@gmail.com>
2023-10-21 18:38:37 +0800

    removed static TutorInfo model

--- app/src/main/java/com/ren/tutornearme/util/Common.java
+++ app/src/main/java/com/ren/tutornearme/util/Common.java
@@ -1,10 +1,7 @@
 package com.ren.tutornearme.util;
 
-import com.ren.tutornearme.model.TutorInfo;
-
 public class Common {
     public static final String TUTOR_INFO_REFERENCE = "TutorInfo";
     public static final double ZAM_LONG = 122.079025;
     public static final double ZAM_LAT = 6.921442;
-    public static TutorInfo currentTutor;
 }
