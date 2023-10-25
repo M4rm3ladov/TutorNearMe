7ba9c8af60b485cf858ed8bf0e78f26468f630be
M4rm3ladov <renier.antonio20@gmail.com>
2023-10-26 00:56:19 +0800

    added tutor info and sign out in drawer

--- app/src/main/java/com/ren/tutornearme/HomeActivity.java
+++ app/src/main/java/com/ren/tutornearme/HomeActivity.java
@@ -1,12 +1,22 @@
 package com.ren.tutornearme;
 
 
+import static com.ren.tutornearme.util.Common.CURRENT_USER;
+
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -14,18 +24,26 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.ren.tutornearme.auth.MainActivity;
 import com.ren.tutornearme.databinding.ActivityHomeBinding;
+import com.ren.tutornearme.model.TutorInfo;
 import com.ren.tutornearme.util.InternetHelper;
 
 public class HomeActivity extends AppCompatActivity{
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityHomeBinding binding;
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+    private Resources res;
+    private TutorInfo tutorInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        tutorInfo = (TutorInfo) getIntent().getSerializableExtra(CURRENT_USER);
         initNetworkAvailability();
         initLayoutBinding();
         initNavigationDrawer();
@@ -45,11 +63,11 @@ public class HomeActivity extends AppCompatActivity{
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarHome.toolbar);
+        drawer = binding.drawerLayout;
     }
 
     private void initNavigationDrawer() {
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
+        navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -59,6 +77,56 @@ public class HomeActivity extends AppCompatActivity{
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        res = getResources();
+        setSignOutBuilder();
+        setNameAndPhoneNumber();
+    }
+
+    private void setSignOutBuilder() {
+        navigationView.getMenu().findItem(R.id.nav_sign_out).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(@NonNull MenuItem menuItem) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this);
+                builder.setTitle("Sign Out")
+                        .setMessage("Do you really wish to sign out?")
+                        .setNegativeButton("Cancel", (dialogInterface, i) -> dialogInterface.dismiss())
+                        .setPositiveButton("Sign Out", (dialogInterface, i) -> {
+                            signOutUser();
+                        })
+                        .setCancelable(false);
+
+                AlertDialog dialog = builder.create();
+                dialog.setOnShowListener(dialogInterface -> {
+                    dialog.getButton(AlertDialog.BUTTON_NEGATIVE)
+                            .setTextColor(res.getColor(R.color.red));
+                    dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+                            .setTextColor(res.getColor(R.color.green));
+                });
+                dialog.show();
+                return false;
+            }
+        });
+    }
+
+    private void signOutUser() {
+        FirebaseAuth.getInstance().signOut();
+
+        Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+    private void setNameAndPhoneNumber() {
+        View headerView = navigationView.getHeaderView(0);
+        TextView tutorName = headerView.findViewById(R.id.name_textview);
+        TextView phoneNumber = headerView.findViewById(R.id.phone_number_textview);
+
+        String name = String.format(res.getString(R.string.tutor_name),
+                tutorInfo.getFirstName(), tutorInfo.getLastName());
+        tutorName.setText(name);
+        phoneNumber.setText(tutorInfo.getPhoneNumber());
     }
 
     @Override
@@ -73,5 +141,13 @@ public class HomeActivity extends AppCompatActivity{
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START))
+            drawer.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
     }
 }
