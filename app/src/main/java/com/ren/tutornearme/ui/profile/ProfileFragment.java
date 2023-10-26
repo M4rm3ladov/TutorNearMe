db8b2c48a7f7e1bcc6a4d0772a0f83248319c6af
M4rm3ladov <renier.antonio20@gmail.com>
2023-10-26 22:59:29 +0800

    removed default text

--- app/src/main/java/com/ren/tutornearme/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/ren/tutornearme/ui/profile/ProfileFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -22,8 +21,6 @@ private FragmentProfileBinding binding;
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
-        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
