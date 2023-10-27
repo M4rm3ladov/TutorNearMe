b5b252fd38c342be91783ec13465e49d834a281a
M4rm3ladov <renier.antonio20@gmail.com>
2023-10-27 21:20:16 +0800

    added profile strings for bundle

--- app/src/main/java/com/ren/tutornearme/util/Common.java
+++ app/src/main/java/com/ren/tutornearme/util/Common.java
@@ -4,6 +4,10 @@ public class Common {
     public static final String TUTOR_INFO_REFERENCE = "TutorInfo";
     public static final String TUTOR_LOCATION_REFERENCE = "TutorLocation";
     public static final String CURRENT_USER = "user";
+    public static final String FIRST_NAME = "FirstName";
+    public static final String LAST_NAME = "LastName";
+    public static final String GENDER = "Gender";
+    public static final String BARANGAY = "Barangay";
     public static final double ZAM_LONG = 122.079025;
     public static final double ZAM_LAT = 6.921442;
 }
