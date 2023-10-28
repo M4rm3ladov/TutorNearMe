8218a1495c321498cf309663acac08abaa3193ff
M4rm3ladov <renier.antonio20@gmail.com>
2023-10-28 18:59:40 +0800

    added firebase storage

--- app/src/main/java/com/ren/tutornearme/ui/profile/ProfileRepository.java
+++ app/src/main/java/com/ren/tutornearme/ui/profile/ProfileRepository.java
@@ -5,15 +5,21 @@ import static com.ren.tutornearme.util.Common.TUTOR_INFO_REFERENCE;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 
 public class ProfileRepository {
     private final FirebaseAuth firebaseAuth;
     private final FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private final FirebaseStorage firebaseStorage;
+    private final StorageReference storageRef;
     private final DatabaseReference tutorInfoRef = db.getReference(TUTOR_INFO_REFERENCE);
 
     public ProfileRepository() {
         firebaseAuth = FirebaseAuth.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageRef = firebaseStorage.getReference();
     }
 
 }
