f11b8b7849516caeaa992d487d29fae6b49731cd
M4rm3ladov <renier.antonio20@gmail.com>
2023-10-30 03:43:15 +0800

    changed min sdk to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ren.tutornearme"
-        minSdk = 22
+        minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
