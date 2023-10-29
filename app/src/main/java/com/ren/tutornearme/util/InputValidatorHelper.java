f16907aed03f1b2a53b8b84ff1b647811d03cf8b
M4rm3ladov <renier.antonio20@gmail.com>
2023-10-30 03:55:26 +0800

    added age validator

--- app/src/main/java/com/ren/tutornearme/util/InputValidatorHelper.java
+++ app/src/main/java/com/ren/tutornearme/util/InputValidatorHelper.java
@@ -2,6 +2,8 @@ package com.ren.tutornearme.util;
 
 import android.text.TextUtils;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -34,6 +36,13 @@ public class InputValidatorHelper {
 
     public boolean isNumeric(String string){
         return TextUtils.isDigitsOnly(string);
+    }
+
+    public boolean isNotLegalAge(int year, int month, int dayOfMonth) {
+        return Period.between(
+                LocalDate.of(year, month, dayOfMonth),
+                LocalDate.now()
+        ).getYears() < 18;
     }
 
     //Add more validators here if necessary
