2da1e4d804caf6f0bceecad2d9901778c5dc776a
M4rm3ladov <renier.antonio20@gmail.com>
2023-10-30 09:28:37 +0800

    added update and upload avatar

--- app/src/main/java/com/ren/tutornearme/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/ren/tutornearme/ui/profile/ProfileViewModel.java
@@ -1,6 +1,13 @@
 package com.ren.tutornearme.ui.profile;
 
+import android.net.Uri;
+
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.ren.tutornearme.data.DataOrException;
+
+import java.util.Map;
 
 public class ProfileViewModel extends ViewModel {
 
@@ -10,5 +17,11 @@ public class ProfileViewModel extends ViewModel {
         this.profileRepository = new ProfileRepository();
     }
 
+    public LiveData<DataOrException<Map<String, Object>, Exception>> uploadAvatar(Uri imageUri) {
+        return profileRepository.uploadAvatar(imageUri);
+    }
 
+    public LiveData<DataOrException<Boolean, Exception>> updateAvatar() {
+        return profileRepository.updateAvatar();
+    }
 }
