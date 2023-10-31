b6ccbef2b5fe74acaab111c609095a342d528138
M4rm3ladov <renier.antonio20@gmail.com>
2023-10-31 21:56:42 +0800

    added valid id uri and string path for persistence

--- app/src/main/java/com/ren/tutornearme/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/ren/tutornearme/ui/profile/ProfileViewModel.java
@@ -3,6 +3,7 @@ package com.ren.tutornearme.ui.profile;
 import android.net.Uri;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.ren.tutornearme.data.DataOrException;
@@ -12,16 +13,36 @@ import java.util.Map;
 public class ProfileViewModel extends ViewModel {
 
     private final ProfileRepository profileRepository;
+    private final MutableLiveData<Uri> validIdUri = new MutableLiveData<>();
+    private final MutableLiveData<String> validIdPath = new MutableLiveData<>();
+    private MutableLiveData<Uri> resume;
 
     public ProfileViewModel() {
         this.profileRepository = new ProfileRepository();
     }
 
-    public LiveData<DataOrException<Map<String, Object>, Exception>> uploadAvatar(Uri imageUri) {
-        return profileRepository.uploadAvatar(imageUri);
+    public LiveData<DataOrException<Map<String, Object>, Exception>> uploadAvatarOrValidId
+            (Uri imageUri, String imageFlag) {
+        return profileRepository.uploadAvatarOrValidId(imageUri, imageFlag);
     }
 
-    public LiveData<DataOrException<Boolean, Exception>> updateAvatar() {
-        return profileRepository.updateAvatar();
+    public LiveData<DataOrException<Boolean, Exception>> updateAvatarOrValidId(String imageFlag, String validIdType) {
+        return profileRepository.updateAvatarOrValidId(imageFlag, validIdType);
+    }
+
+    public MutableLiveData<String> getValidIdPath() {
+        return validIdPath;
+    }
+
+    public void setValidIdPath(String path) {
+        validIdPath.setValue(path);
+    }
+
+    public void setValidIdUri(Uri uri) {
+        validIdUri.setValue(uri);
+    }
+
+    public MutableLiveData<Uri> getValidIdUri() {
+        return validIdUri;
     }
 }
