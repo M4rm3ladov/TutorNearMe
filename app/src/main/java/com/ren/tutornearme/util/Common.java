9a0a166ef3c6125a46d577eabdff57996c25244b
M4rm3ladov <renier.antonio20@gmail.com>
2023-11-06 02:16:18 +0800

    added firebase messaging constants

--- app/src/main/java/com/ren/tutornearme/util/Common.java
+++ app/src/main/java/com/ren/tutornearme/util/Common.java
@@ -3,13 +3,15 @@ package com.ren.tutornearme.util;
 public class Common {
     public static final String TUTOR_INFO_REFERENCE = "TutorInfo";
     public static final String TUTOR_LOCATION_REFERENCE = "TutorLocation";
+    public static final String TOKEN_REFERENCE = "Token";
+    public static final String NOTIFICATION_TITLE = "title";
+    public static final String NOTIFICATION_BODY = "body";
     public static final String CURRENT_USER = "user";
     public static final String FIRST_NAME = "FirstName";
     public static final String LAST_NAME = "LastName";
     public static final String GENDER = "Gender";
     public static final String BARANGAY = "Barangay";
     public static final String BIRTH_DATE = "BirthDate";
-    public static final String TUTOR_CREATED_DATE = "TutorCreatedDate";
     public static final double ZAM_LONG = 122.079025;
     public static final double ZAM_LAT = 6.921442;
 }
