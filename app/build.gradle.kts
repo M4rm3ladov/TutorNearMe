cc1d1a0500bdb2c2cb6d09638a674ea1cb8508c7
M4rm3ladov <renier.antonio20@gmail.com>
2023-12-14 19:51:37 +0800

    added circle image view dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,9 @@ dependencies {
     // image preview
     implementation("com.github.stfalcon-studio:StfalconImageViewer:v1.0.1")
 
+    // circle image
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
