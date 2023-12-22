1f05360bc0010aafd1785191efdeac9da4b09a9d
M4rm3ladov <renier.antonio20@gmail.com>
2023-12-22 19:02:35 +0800

    added back arrow press fragment navigation

--- app/src/main/java/com/ren/tutornearme/ui/subject/SubjectRequestActivity.java
+++ app/src/main/java/com/ren/tutornearme/ui/subject/SubjectRequestActivity.java
@@ -3,6 +3,8 @@ package com.ren.tutornearme.ui.subject;
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -27,8 +29,17 @@ public class SubjectRequestActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == android.R.id.home)
-            finish();
+        NavController navController = Navigation.findNavController(this, R.id.subject_host_fragment);
+        if (navController.getCurrentDestination() == null) finish();
+        int id = navController.getCurrentDestination().getId();
+
+        if (item.getItemId() == android.R.id.home) {
+            if (id == R.id.subjectFilesFragment)
+                Navigation.findNavController(findViewById(R.id.subject_host_fragment))
+                        .navigate(R.id.subjectListFragment);
+            else
+                finish();
+        }
         return super.onOptionsItemSelected(item);
     }
 }
