125f18c19cc39417a0b9f5e11e125d699218858a
M4rm3ladov <renier.antonio20@gmail.com>
2023-12-22 19:00:35 +0800

    added tutor subject look up ref

--- app/src/main/java/com/ren/tutornearme/util/Common.java
+++ app/src/main/java/com/ren/tutornearme/util/Common.java
@@ -5,6 +5,7 @@ public class Common {
     public static final String TUTOR_LOCATION_REFERENCE = "TutorLocation";
     public static final String SUBJECT_REFERENCE = "SubjectInfo";
     public static final String TUTOR_SUBJECT_REFERENCE = "TutorSubject";
+    public static final String TUTOR_SUBJECT_LOOKUP_REFERENCE = "TutorSubjectLookUp";
     public static final String TOKEN_REFERENCE = "MessagingToken";
     public static final String NOTIFICATION_TITLE = "title";
     public static final String NOTIFICATION_BODY = "body";
