9c675737b326ab5f7b128da5990666017cd12215
M4rm3ladov <renier.antonio20@gmail.com>
2024-01-12 16:36:45 +0800

    changed list from subject info to tutor subject and added enabling subject functionality

--- app/src/main/java/com/ren/tutornearme/adapter/TutorSubjectAdapter.java
+++ app/src/main/java/com/ren/tutornearme/adapter/TutorSubjectAdapter.java
@@ -1,18 +1,33 @@
 package com.ren.tutornearme.adapter;
 
+import android.app.Application;
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.cardview.widget.CardView;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.materialswitch.MaterialSwitch;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputLayout;
 import com.ren.tutornearme.R;
-import com.ren.tutornearme.model.SubjectInfo;
+import com.ren.tutornearme.model.TutorSubject;
+import com.ren.tutornearme.ui.subject.tutor_subject.TutorSubjectViewModel;
+import com.ren.tutornearme.util.InputValidatorHelper;
+import com.ren.tutornearme.util.InternetHelper;
+import com.ren.tutornearme.util.SnackBarHelper;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -21,15 +36,29 @@ import java.util.Locale;
 
 public class TutorSubjectAdapter extends RecyclerView.Adapter<TutorSubjectAdapter.SubjectViewHolder>{
 
+    private final TutorSubjectViewModel tutorSubjectViewModel;
+    private final LifecycleOwner lifecycleOwner;
     private final Context context;
     private View view;
 
-    private ArrayList<SubjectInfo> subjectInfoArrayList = new ArrayList<>();
+    private AlertDialog alertDialog;
+    private Button saveButton;
+    private ImageView backImageView;
+    private EditText tutorHourEditText;
+    private TextInputLayout tutorHourInputLayout;
+    private boolean isValid = true;
+
+    private final InputValidatorHelper inputValidatorHelper = new InputValidatorHelper();
+
+
+    private ArrayList<TutorSubject> tutorSubjectArrayList = new ArrayList<>();
     private final SimpleDateFormat dateTimeFormatter =
             new SimpleDateFormat( "dd-MMM-yyyy, hh:mm" , Locale.ENGLISH);
 
-    public TutorSubjectAdapter(Context context) {
+    public TutorSubjectAdapter(Context context, TutorSubjectViewModel tutorSubjectViewModel, LifecycleOwner lifecycleOwner) {
         this.context = context;
+        this.tutorSubjectViewModel = tutorSubjectViewModel;
+        this.lifecycleOwner = lifecycleOwner;
     }
 
     @NonNull
@@ -41,17 +70,149 @@ public class TutorSubjectAdapter extends RecyclerView.Adapter<TutorSubjectAdapte
 
     @Override
     public void onBindViewHolder(@NonNull SubjectViewHolder holder, int position) {
-        SubjectInfo subjectInfo = subjectInfoArrayList.get(position);
-        holder.subjectNameTextView.setText(subjectInfo.getName());
-        holder.subjectDescriptionTextView.setText(subjectInfo.getDescription());
+        TutorSubject tutorSubject = tutorSubjectArrayList.get(position);
+        holder.subjectNameTextView.setText(tutorSubject.getSubjectInfo().getName());
+        holder.subjectDescriptionTextView.setText(tutorSubject.getSubjectInfo().getDescription());
         holder.updatedDateTextView
-                .setText(dateTimeFormatter.format(new Date(subjectInfo.getUpdatedDate())));
+                .setText(dateTimeFormatter.format(new Date(tutorSubject.getSubjectInfo().getUpdatedDate())));
         holder.logoImageView.setBackgroundResource(R.mipmap.ic_logo_round);
+        holder.subjectSwitch.setChecked(tutorSubject.isAvailable());
+
+        holder.rowCardView.setOnClickListener(view -> {
+            createUpdateDialogBuilder(position);
+        });
+
+        holder.subjectSwitch.setOnCheckedChangeListener((compoundButton, isChecked) -> {
+            saveSubjectAvailability(position, isChecked);
+        });
+    }
+
+    private void saveSubjectAvailability(int position, boolean isChecked) {
+
+        String key = tutorSubjectArrayList.get(position).getId();
+
+        tutorSubjectViewModel.setTutorSubjectAvailability(key, isChecked)
+                .observe(lifecycleOwner, dataOrException -> {
+                    if (dataOrException.exception != null) {
+                        SnackBarHelper.showSnackBar(view, dataOrException.exception.getMessage());
+                        return;
+                    }
+
+                    if (dataOrException.data != null) {
+                        Toast.makeText(context, "Subject enabled.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void createUpdateDialogBuilder(int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        View tutorHourDialogView = LayoutInflater.from(context).inflate(R.layout.dialog_tutor_hour, null);
+        initBindDialogViews(tutorHourDialogView);
+        initAttachButtonListeners(position);
+        initAttachInputListeners();
+
+        builder.setView(tutorHourDialogView);
+        builder.setCancelable(false);
+        alertDialog = builder.create();
+        if (alertDialog.getWindow() != null)
+            alertDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        alertDialog.show();
+
+    }
+
+    private void initBindDialogViews(View view) {
+        tutorHourEditText = view.findViewById(R.id.tutor_hour_editText);
+        saveButton = view.findViewById(R.id.tutor_hour_saveButton);
+        backImageView = view.findViewById(R.id.tutor_hour_imageView);
+        tutorHourInputLayout = view.findViewById(R.id.tutor_hour_inputLayout);
+    }
+
+    private void initAttachButtonListeners(int position) {
+        saveButton.setOnClickListener(view -> {
+            if (!InternetHelper.isOnline((Application) context.getApplicationContext())) {
+                Snackbar.make(view,
+                        "[ERROR]: No internet connection. Please check your network",
+                        Snackbar.LENGTH_SHORT).show();
+                return;
+            }
+            saveTutorSubjectSession(position);
+        });
+        backImageView.setOnClickListener(view -> {
+            alertDialog.dismiss();
+        });
+    }
+
+    private void saveTutorSubjectSession(int position) {
+        validateBeforeSave();
+        if (!isValid) return;
+
+        int sessionHours = Integer.parseInt(tutorHourEditText.getText().toString().trim());
+        String key = tutorSubjectArrayList.get(position).getId();
+
+        tutorSubjectViewModel.setTutorSubjectSession(key, sessionHours)
+                .observe(lifecycleOwner, dataOrException -> {
+                    if (dataOrException.exception != null) {
+                        SnackBarHelper.showSnackBar(view, dataOrException.exception.getMessage());
+                        return;
+                    }
+
+                    if (dataOrException.data != null) {
+                        Toast.makeText(context, "Saved successfully",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    alertDialog.dismiss();
+                });
+    }
+
+    private void validateBeforeSave() {
+        String tutorHour = tutorHourEditText.getText().toString().trim();
+        isValid = true;
+
+        if (inputValidatorHelper.isNullOrEmpty(tutorHour)) {
+            tutorHourInputLayout.setHelperText("Fill in session.");
+            isValid = false;
+        } else if (!inputValidatorHelper.isNumeric(tutorHour)) {
+            tutorHourInputLayout.setHelperText("Fill in a valid hour.");
+            isValid = false;
+        }
+
+    }
+
+    private void initAttachInputListeners() {
+        tutorHourEditText.setOnFocusChangeListener((view, isFocused) -> {
+
+            String tutorHour = tutorHourEditText.getText().toString().trim();
+            if (!isFocused) {
+                if (inputValidatorHelper.isNullOrEmpty(tutorHour))
+                    tutorHourInputLayout.setHelperText("Fill in session.");
+                else if (!inputValidatorHelper.isNumeric(tutorHour))
+                    tutorHourInputLayout.setHelperText("Fill in a valid hour.");
+            }
+        });
+
+        tutorHourEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                tutorHourInputLayout.setHelperText("");
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return subjectInfoArrayList.size();
+        return tutorSubjectArrayList.size();
     }
 
     public static class SubjectViewHolder extends RecyclerView.ViewHolder {
@@ -59,6 +220,7 @@ public class TutorSubjectAdapter extends RecyclerView.Adapter<TutorSubjectAdapte
         private final TextView subjectNameTextView, subjectDescriptionTextView, updatedDateTextView;
         private final ImageView logoImageView;
         private final CardView rowCardView;
+        private final MaterialSwitch subjectSwitch;
         public SubjectViewHolder(@NonNull View itemView) {
             super(itemView);
 
@@ -67,11 +229,12 @@ public class TutorSubjectAdapter extends RecyclerView.Adapter<TutorSubjectAdapte
             updatedDateTextView = itemView.findViewById(R.id.tutor_subject_updated_date);
             logoImageView = itemView.findViewById(R.id.tutor_subject_logo_imageView);
             rowCardView = itemView.findViewById(R.id.tutor_subject_row_cardView);
+            subjectSwitch = itemView.findViewById(R.id.tutor_subject_switch);
         }
     }
 
-    public void setSubjectList(ArrayList<SubjectInfo> filteredList) {
-        subjectInfoArrayList = filteredList;
+    public void setTutorSubjectList(ArrayList<TutorSubject> filteredList) {
+        tutorSubjectArrayList = filteredList;
         notifyDataSetChanged();
     }
 }
