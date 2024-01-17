ebeeac7acfaee6e7f807e63b81b0b847e1fe7be1
M4rm3ladov <renier.antonio20@gmail.com>
2024-01-18 00:55:35 +0800

    added student request listener and tutor working location updated and mutable livedata boolean isTutorWorking

--- app/src/main/java/com/ren/tutornearme/ui/home/HomeRepository.java
+++ app/src/main/java/com/ren/tutornearme/ui/home/HomeRepository.java
@@ -1,7 +1,12 @@
 package com.ren.tutornearme.ui.home;
 
 import static com.ren.tutornearme.util.Common.TUTOR_LOCATION_REFERENCE;
+import static com.ren.tutornearme.util.Common.TUTOR_REQUEST_REFERENCE;
+import static com.ren.tutornearme.util.Common.TUTOR_WORKING_REFERENCE;
+
 import android.location.Location;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 
 import com.firebase.geofire.GeoFire;
@@ -9,9 +14,11 @@ import com.firebase.geofire.GeoLocation;
 import com.google.android.gms.location.LocationResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.ren.tutornearme.data.DataOrException;
 
 
@@ -24,19 +31,31 @@ public class HomeRepository {
     private DatabaseReference currentUserRef;
     private GeoFire geoFire;
 
+    private final DatabaseReference tutorRequestRef;
+    private final DatabaseReference tutorWorkingRef;
+    private ValueEventListener tutorRequestRefListener = null;
+
 
     public HomeRepository() {
         firebaseAuth = FirebaseAuth.getInstance();
         currentUser = firebaseAuth.getCurrentUser();
         geoFire = new GeoFire(db.getReference(TUTOR_LOCATION_REFERENCE));
+        tutorRequestRef = db.getReference(TUTOR_REQUEST_REFERENCE);
+        tutorWorkingRef = db.getReference(TUTOR_WORKING_REFERENCE);
     }
 
     public FirebaseUser getCurrentUser() {
         return currentUser;
     }
 
+    public void removeTutorRequestListener() {
+        if (tutorRequestRefListener != null)
+            tutorRequestRef.removeEventListener(tutorRequestRefListener);
+    }
+
     public void removeTutorLocation() {
-        geoFire.removeLocation(currentUser.getUid());
+        GeoFire geoFire = new GeoFire(tutorLocationRef);
+        geoFire.removeLocation(getCurrentUser().getUid());
     }
 
     public MutableLiveData<DataOrException<Boolean, Exception>> checkTutorLocationSet(LocationResult locationResult) {
@@ -83,9 +102,91 @@ public class HomeRepository {
         return mutableLiveData;
     }
 
+    public MutableLiveData<DataOrException<Boolean, Exception>> removeTutorWorking() {
+        MutableLiveData<DataOrException<Boolean, Exception>> mutableLiveData = new MutableLiveData<>();
+        DataOrException<Boolean, Exception> dataOrException = new DataOrException<>();
+
+        if (getCurrentUser() != null) {
+            tutorWorkingRef.child(getCurrentUser().getUid()).removeValue()
+                    .addOnCompleteListener(task -> {
+                        dataOrException.data = task.isSuccessful();
+                        mutableLiveData.postValue(dataOrException);
+                    }).addOnFailureListener(e -> {
+                        dataOrException.exception = e;
+                        mutableLiveData.postValue(dataOrException);
+                    });
+        }
+        return mutableLiveData;
+    }
+
+    public MutableLiveData<DataOrException<Boolean, Exception>> removeTutorRequest() {
+        MutableLiveData<DataOrException<Boolean, Exception>> mutableLiveData = new MutableLiveData<>();
+        DataOrException<Boolean, Exception> dataOrException = new DataOrException<>();
+
+        if (getCurrentUser() != null) {
+            tutorRequestRef.child(getCurrentUser().getUid()).removeValue()
+                    .addOnCompleteListener(task -> {
+                        dataOrException.data = task.isSuccessful();
+                        mutableLiveData.postValue(dataOrException);
+                    }).addOnFailureListener(e -> {
+                        dataOrException.exception = e;
+                        mutableLiveData.postValue(dataOrException);
+                    });
+        }
+        return mutableLiveData;
+    }
+
+    public MutableLiveData<DataOrException<Boolean, Exception>> checkStudentRequest(Location location) {
+        MutableLiveData<DataOrException<Boolean, Exception>> mutableLiveData = new MutableLiveData<>();
+        DataOrException<Boolean, Exception> dataOrException = new DataOrException<>();
+
+        if (getCurrentUser() != null) {
+            tutorRequestRefListener = tutorRequestRef
+                    .addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.child(getCurrentUser().getUid()).exists()) {
+                        dataOrException.data = true;
+                        mutableLiveData.postValue(dataOrException);
+                    } else {
+                        dataOrException.data = false;
+                        mutableLiveData.postValue(dataOrException);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    dataOrException.exception = error.toException();
+                    mutableLiveData.postValue(dataOrException);
+                }
+            });
+        }
+        return mutableLiveData;
+    }
+
+    public MutableLiveData<DataOrException<Boolean, Exception>> updateTutorWorkingLocation(Location location) {
+        MutableLiveData<DataOrException<Boolean, Exception>> mutableLiveData = new MutableLiveData<>();
+        DataOrException<Boolean, Exception> dataOrException = new DataOrException<>();
+
+        geoFire = new GeoFire(tutorWorkingRef);
+            geoFire.setLocation(currentUser.getUid(),
+                    new GeoLocation(location.getLatitude(),
+                            location.getLongitude()), (key, error) -> {
+                if (error != null)
+                    dataOrException.exception = error.toException();
+                else
+                    dataOrException.data = true;
+                mutableLiveData.postValue(dataOrException);
+
+        });
+            return mutableLiveData;
+    }
+
     public DatabaseReference getOnlineRef() { return onlineRef; }
 
     public DatabaseReference getCurrentUserRef() { return currentUserRef; }
+    public DatabaseReference getTutorWorkingRef() { return tutorWorkingRef; }
+    public DatabaseReference getTutorRequestRef() { return tutorRequestRef; }
 
     public void setCurrentTutorLocationRef(String cityName) {
         tutorLocationRef = db.getReference(TUTOR_LOCATION_REFERENCE).child(cityName);
--- app/src/main/java/com/ren/tutornearme/ui/home/HomeViewModel.java
+++ app/src/main/java/com/ren/tutornearme/ui/home/HomeViewModel.java
@@ -4,6 +4,7 @@ import android.graphics.drawable.Drawable;
 import android.location.Location;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.google.android.gms.location.LocationResult;
@@ -13,9 +14,18 @@ import com.ren.tutornearme.data.DataOrException;
 public class HomeViewModel extends ViewModel {
     private final HomeRepository homeRepository;
     private Drawable locationButtonImage;
+    private final MutableLiveData<Boolean> isTutorWorking = new MutableLiveData<>();
 
     public HomeViewModel() {
         this.homeRepository = new HomeRepository();
+    }
+
+    public MutableLiveData<Boolean> getIsTutorWorking() {
+        return isTutorWorking;
+    }
+
+    public void setIsTutorWorking(Boolean isTutorWorking) {
+        this.isTutorWorking.postValue(isTutorWorking);
     }
 
     public void setLocationButtonImage(Drawable locationButtonImage) {
@@ -30,6 +40,10 @@ public class HomeViewModel extends ViewModel {
         homeRepository.removeTutorLocation();
     }
 
+    public void removeCustomerRequestListener() {
+        homeRepository.removeTutorRequestListener();
+    }
+
     public LiveData<DataOrException<Boolean, Exception>> isTutorLocationSet(LocationResult locationResult) {
         return homeRepository.checkTutorLocationSet(locationResult);
     }
@@ -38,7 +52,26 @@ public class HomeViewModel extends ViewModel {
         return homeRepository.checkTutorLocationSet(location);
     }
 
+    public LiveData<DataOrException<Boolean, Exception>> removeTutorWorking() {
+        return homeRepository.removeTutorWorking();
+    }
+
+    public LiveData<DataOrException<Boolean, Exception>> removeTutorRequest() {
+        return homeRepository.removeTutorRequest();
+    }
+
+    public LiveData<DataOrException<Boolean, Exception>> checkStudentRequest(Location location) {
+        return homeRepository.checkStudentRequest(location);
+    }
+
+    public LiveData<DataOrException<Boolean, Exception>> updateTutorWorkingLocation(Location location) {
+        return homeRepository.updateTutorWorkingLocation(location);
+    }
+
     public DatabaseReference getCurrentUserRef() { return homeRepository.getCurrentUserRef(); }
+
+    public DatabaseReference getTutorWorkingRef() { return homeRepository.getTutorWorkingRef(); }
+    public DatabaseReference getTutorRequestRef() { return homeRepository.getTutorRequestRef(); }
 
     public void setTutorLocationRef(String cityName) {
         homeRepository.setCurrentTutorLocationRef(cityName);
