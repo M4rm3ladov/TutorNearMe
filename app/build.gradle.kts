0d23b0b0af0fd2ec118b12280a45f3d7df28f7f6
M4rm3ladov <renier.antonio20@gmail.com>
2024-01-18 18:52:41 +0800

    added maps route library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,6 +85,9 @@ dependencies {
     // circle image
     implementation("de.hdodenhof:circleimageview:3.1.0")
 
+    // map routes
+    implementation("com.github.jd-alexander:library:1.1.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
